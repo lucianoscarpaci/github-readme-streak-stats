db68b11b630c5648e2ee509fafd03e62b1e60809
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-12-02 19:44:44 -0500

    Update Relative Path

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 // load functions
-require_once "../vendor/autoload.php";
+require_once __DIR__ . "/../vendor/autoload.php";
 require_once "stats.php";
 require_once "card.php";
 
